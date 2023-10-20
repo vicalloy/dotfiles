7c546cbef22ba87f5231ccdbac24af1a92fe7a8a
vicalloy <zbirder@gmail.com>
2023-10-20 17:11:25 +0800

    add kevinhwang91/nvim-ufo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,6 +20,7 @@ vim.api.nvim_set_option('backup', false)
 vim.api.nvim_set_option('fileformats', 'unix')
 vim.api.nvim_set_option('encoding', 'utf-8')
 vim.api.nvim_set_option('fileencodings', 'ucs-bom,utf-8,gbk,big5,latin1')
+vim.o.foldlevel = 99
 
 -- neovide
 if vim.g.neovide then
@@ -295,6 +296,16 @@ require("lazy").setup(
       require("nvim-surround").setup({
       })
     end
-  }
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    event = "VeryLazy",
+    lazy = true,
+    dependencies = "kevinhwang91/promise-async",
+    config = function()
+      require("ufo").setup({
+      })
+    end
+  },
 })
 
